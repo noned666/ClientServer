6eb1fd41a75f94dbbb454149864d946824bce067
Константин Калмыков <Kleymoments@gmail.com>
2022-08-22 10:12:08 +0300

    Добавьте файлы проекта.

---
+++ ClientServer/Program.cs
@@ -0,0 +1,14 @@
+using ClientServer;   // пространство имен класса ChatHub
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();      // подключема сервисы SignalR
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapHub<ChatHub>("/chat");   // ChatHub будет обрабатывать запросы по пути /chat
+
+app.Run();
